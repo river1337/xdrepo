f7604202634b71f52407116eca5942d8c6f2d428
river <161112493+river1337@users.noreply.github.com>
2024-07-07 21:12:38 -0500

    Update utils.lua

--- utils.lua
+++ utils.lua
@@ -1,16 +1,18 @@
-function EnemyTarget()
-    local entities = world.entities()
-    local target = nil
-    for i = 1, #entities do
-        if entities[i] ~= player.id() and world.is_player(entities[i]) then
-            if target == nil then
-                target = entities[i]
-            else
-                if player.distance_to_entity(entities[i]) < player.distance_to_entity(target) then
+local utils = { 
+    function EnemyTarget()
+        local entities = world.entities()
+        local target = nil
+        for i = 1, #entities do
+            if entities[i] ~= player.id() and world.is_player(entities[i]) then
+                if target == nil then
                     target = entities[i]
+                else
+                    if player.distance_to_entity(entities[i]) < player.distance_to_entity(target) then
+                        target = entities[i]
+                    end
                 end
             end
         end
+        return target
     end
-    return target
-end
+}
